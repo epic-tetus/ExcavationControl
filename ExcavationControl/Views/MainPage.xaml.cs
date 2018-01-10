64e637ee44ecd3d1c46ece2ee9989ba409512914
Dongyoung_Kim <521ab@naver.com>
2018-01-10 11:11:33 +0900

    시리얼 통신 부분 추가

---
+++ ExcavationControl/Views/MainPage.xaml.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcavationControl.Views
+{
+    /// <summary>
+    /// MainPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            HCKnob.knob.ValueChanged += HCKnob_ValueChanged;
+            SCKnob.knob.ValueChanged += SCKnob_ValueChanged;
+            CBKnob.knob.ValueChanged += CBKnob_ValueChanged;
+            EXKnob.knob.ValueChanged += EXKnob_ValueChanged;
+        }
+
+        private void HCKnob_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)Math.Round(HCKnob.knob.Value);
+
+            if (value <= 120 && value >= 101)
+                return;
+
+            HText.Text = Math.Round(HCKnob.knob.Value).ToString();
+
+        }
+
+        private void SCKnob_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)Math.Round(SCKnob.knob.Value);
+
+            if (value <= 120 && value >= 101)
+                return;
+
+            SText.Text = Math.Round(SCKnob.knob.Value).ToString();
+        }
+
+        private void CBKnob_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)Math.Round(CBKnob.knob.Value);
+
+            if (value <= 120 && value >= 101)
+                return;
+
+            CText.Text = Math.Round(CBKnob.knob.Value).ToString();
+        }
+
+        private void EXKnob_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)Math.Round(EXKnob.knob.Value);
+
+            if (value <= 120 && value >= 101)
+                return;
+
+            EText.Text = Math.Round(EXKnob.knob.Value).ToString();
+        }
+
+        private void HUp_Click(object sender, RoutedEventArgs e)
+        {
+            int baseValue = int.Parse(HText.Text);
+
+            HText.Text = (++baseValue).ToString();
+        }
+
+        private void HDown_Click(object sender, RoutedEventArgs e)
+        {
+            int baseValue = int.Parse(HText.Text);
+
+            HText.Text = (--baseValue).ToString();
+        }
+
+        private void HButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            Button selectedButton = (Button)sender;
+            switch (selectedButton.Uid)
+            {
+                case "2":
+                    Debug.WriteLine("Right Button");
+                    break;
+
+                case "3":
+                    Debug.WriteLine("Left Button");
+                    break;
+
+                case "4":
+                    Debug.WriteLine("Stop Button");
+                    break;
+
+                case "5":
+                    Debug.WriteLine("Start Button");
+                    break;
+            }
+        }
+
+        private void SButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            int baseValue = int.Parse(SText.Text);
+
+            Button selectedButton = (Button)sender;
+            switch (selectedButton.Uid)
+            {
+                case "0":
+                    Debug.WriteLine("Up Button");
+                    SText.Text = (++baseValue).ToString();
+                    break;
+
+                case "1":
+                    Debug.WriteLine("Down Button");
+                    SText.Text = (--baseValue).ToString();
+                    break;
+
+                case "2":
+                    Debug.WriteLine("Right Button");
+                    break;
+
+                case "3":
+                    Debug.WriteLine("Left Button");
+                    break;
+
+                case "4":
+                    Debug.WriteLine("Stop Button");
+                    break;
+
+                case "5":
+                    Debug.WriteLine("Start Button");
+                    break;
+            }
+        }
+
+        private void CButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            int baseValue = int.Parse(SText.Text);
+
+            Button selectedButton = (Button)sender;
+            switch (selectedButton.Uid)
+            {
+                case "0":
+                    Debug.WriteLine("Up Button");
+                    CText.Text = (++baseValue).ToString();
+                    break;
+
+                case "1":
+                    Debug.WriteLine("Down Button");
+                    CText.Text = (--baseValue).ToString();
+                    break;
+
+                case "2":
+                    Debug.WriteLine("Right Button");
+                    break;
+
+                case "3":
+                    Debug.WriteLine("Left Button");
+                    break;
+
+                case "4":
+                    Debug.WriteLine("Stop Button");
+                    break;
+
+                case "5":
+                    Debug.WriteLine("Start Button");
+                    break;
+            }
+        }
+
+        private void EButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            int baseValue = int.Parse(SText.Text);
+
+            Button selectedButton = (Button)sender;
+            switch (selectedButton.Uid)
+            {
+                case "0":
+                    Debug.WriteLine("Up Button");
+                    EText.Text = (++baseValue).ToString();
+                    break;
+
+                case "1":
+                    Debug.WriteLine("Down Button");
+                    EText.Text = (--baseValue).ToString();
+                    break;
+
+                case "2":
+                    Debug.WriteLine("Right Button");
+                    break;
+
+                case "3":
+                    Debug.WriteLine("Left Button");
+                    break;
+
+                case "4":
+                    Debug.WriteLine("Stop Button");
+                    break;
+
+                case "5":
+                    Debug.WriteLine("Start Button");
+                    break;
+            }
+        }
+
+        private void LPButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            Button selectedButton = (Button)sender;
+            switch (selectedButton.Uid)
+            {
+                case "0":
+                    Debug.WriteLine("Up Button");
+                    break;
+
+                case "1":
+                    Debug.WriteLine("Down Button");
+                    break;
+
+                case "2":
+                    Debug.WriteLine("Right Button");
+                    break;
+
+                case "3":
+                    Debug.WriteLine("Left Button");
+                    break;
+
+                case "4":
+                    Debug.WriteLine("Angle Button");
+                    break;
+
+                case "5":
+                    Debug.WriteLine("Send Button");
+                    break;
+            }
+        }
+    }
+}
---
+++ ExcavationControl/Views/SettingPage.xaml.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcavationControl.Views
+{
+    /// <summary>
+    /// SettingPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingPage : Page
+    {
+        private SerialPort serial;
+
+        public string _PortName { get; set; }
+        public int _BaudRate { get; set; }
+        public int _DataBits { get; set; }
+        public Parity _Parity { get; set; }
+        public StopBits _StopBits { get; set; }
+        public Handshake _FlowControl { get; set; }
+
+        public SettingPage()
+        {
+
+            InitializeComponent();
+
+            serial = new SerialPort();
+
+            BaudRateCombo.SelectedIndex = 7;
+            DataBitsCombo.SelectedIndex = 3;
+            ParityCombo.SelectedIndex = 0;
+            StopBitsCombo.SelectedIndex = 0;
+            FlowCtrlCombo.SelectedIndex = 0;
+        }
+
+        public SettingPage(SerialPort serial)
+        {
+            InitializeComponent();
+
+            this.serial = serial;
+
+            PortCombo.SelectedIndex = 0;
+            BaudRateCombo.SelectedIndex = 7;
+            DataBitsCombo.SelectedIndex = 3;
+            ParityCombo.SelectedIndex = 0;
+            StopBitsCombo.SelectedIndex = 0;
+            FlowCtrlCombo.SelectedIndex = 0;
+        }
+
+        private void SerialInit()
+        {
+            try
+            {
+                serial.PortName = _PortName;
+                serial.BaudRate = _BaudRate;
+                serial.DataBits = _DataBits;
+                serial.Parity = _Parity;
+                serial.StopBits = _StopBits;
+                serial.Handshake = _FlowControl;
+
+                serial.Open();
+
+                serial.DataReceived += Serial_DataReceived;
+
+                Debug.WriteLine("포트열기 성공!");
+
+                MainPage mainPage = new MainPage();
+
+                NavigationService.Navigate(mainPage);
+
+                //MainWindow._Serial.PortName = serial.PortName;
+                //MainWindow._Serial.BaudRate = serial.BaudRate;
+                //MainWindow._Serial.DataBits = serial.DataBits;
+                //MainWindow._Serial.Parity = serial.Parity;
+                //MainWindow._Serial.StopBits = serial.StopBits;
+                //MainWindow._Serial.Handshake = serial.Handshake;
+            }
+            catch
+            {
+                string Message = "포트 연결에 실패 했습니다.";
+                string Title = "경고!";
+                MessageBoxImage image = MessageBoxImage.Warning;
+                MessageBoxButton button = MessageBoxButton.OK;
+
+                MessageBox.Show(App.Current.MainWindow, Message, Title, button, image);
+
+                Debug.WriteLine("포트 열기 실패!");
+
+                //MainWindow.flag = false;
+            }
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //string recievedData;
+            //recievedData = serial.ReadExisting();
+            //Debug.WriteLine("받은 데이터 : " + recievedData);
+        }
+
+        private void PortCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                _PortName = PortCombo.SelectedItem.ToString();
+            }
+            catch (NullReferenceException nre)
+            {
+                PortCombo.SelectedIndex = 0;
+            }
+
+        }
+
+        private void BaudRateCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _BaudRate = int.Parse(((ComboBoxItem)BaudRateCombo.SelectedValue).Content.ToString());
+            Debug.WriteLine("_BaudRate : " + _BaudRate);
+        }
+
+        private void DataBitsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _DataBits = int.Parse(((ComboBoxItem)DataBitsCombo.SelectedValue).Content.ToString());
+            Debug.WriteLine("_DataBits : " + _DataBits);
+        }
+
+        private void ParityCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (ParityCombo.SelectedIndex)
+            {
+                case 0:
+                    _Parity = Parity.None;
+                    Debug.WriteLine("_Parity None");
+                    break;
+
+                case 1:
+                    _Parity = Parity.Odd;
+                    Debug.WriteLine("_Parity Odd");
+                    break;
+
+                case 2:
+                    _Parity = Parity.Even;
+                    Debug.WriteLine("_Parity Even");
+                    break;
+            }
+        }
+
+        private void StopBitsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (StopBitsCombo.SelectedIndex)
+            {
+                case 0:
+                    _StopBits = StopBits.One;
+                    Debug.WriteLine("_StopBits One");
+                    break;
+
+                case 1:
+                    _StopBits = StopBits.OnePointFive;
+                    Debug.WriteLine("_StopBits OnePointFive");
+                    break;
+
+                case 2:
+                    _StopBits = StopBits.Two;
+                    Debug.WriteLine("_StopBits Two");
+                    break;
+            }
+        }
+
+        private void FlowCtrlCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (FlowCtrlCombo.SelectedIndex)
+            {
+                case 0:
+                    _FlowControl = Handshake.None;
+                    Debug.WriteLine("_FlowControl None");
+                    break;
+
+                case 1:
+                    _FlowControl = Handshake.XOnXOff;
+                    Debug.WriteLine("_FlowControl XonXoff");
+                    break;
+
+                case 2:
+                    _FlowControl = Handshake.RequestToSend;
+                    Debug.WriteLine("_FlowControl RTS");
+                    break;
+            }
+        }
+
+        private void PortCombo_DropDownOpened(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked");
+
+            string[] AvailablePorts = SerialPort.GetPortNames();
+
+            PortCombo.Items.Clear();
+
+            foreach (var data in AvailablePorts)
+            {
+                if (!GetOpenedPortNames().Contains(data))
+                {
+                    Debug.WriteLine("Available Port : " + data);
+                    PortCombo.Items.Add(data);
+                }
+            }
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            SerialInit();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private List<string> GetOpenedPortNames()
+        {
+            List<string> list = new List<string>();
+
+            foreach (var data in SerialPort.GetPortNames())
+            {
+                try
+                {
+                    SerialPort testSerial = new SerialPort(data);
+
+                    testSerial.Open();
+
+                    testSerial.Close();
+                }
+                catch
+                {
+                    Debug.WriteLine("Not Available Port : " + data);
+
+                    list.Add(data);
+                }
+            }
+
+            return list;
+        }
+    }
+}
